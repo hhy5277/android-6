0b4040abbea57dc38555d418c5b8d18219aab359
Andy Scherzinger <info@andy-scherzinger.de>
2019-03-22 15:26:19 +0100

    use correct icon for user account settings within drawer menu
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
@@ -721,7 +721,7 @@ public abstract class DrawerActivity extends ToolbarActivity implements DisplayU
                 getResources().getString(R.string.prefs_add_account)).setIcon(R.drawable.ic_account_plus);
         mNavigationView.getMenu().add(R.id.drawer_menu_accounts, R.id.drawer_menu_account_manage,
                 MENU_ORDER_ACCOUNT_FUNCTION,
-                getResources().getString(R.string.drawer_manage_accounts)).setIcon(R.drawable.settings);
+                getResources().getString(R.string.drawer_manage_accounts)).setIcon(R.drawable.nav_settings);
 
         // adding sets menu group back to visible, so safety check and setting invisible
         showMenu();
